81ceb9ce3a0820f8a47ac7653bb44d047e543a2f
Charrrbel <161362693+Charrrbel@users.noreply.github.com>
2024-04-04 19:51:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,34 +1,51 @@
 import time
 import pygame
 import openpyxl
+import random
+import numpy as np
 
 pygame.init()
+t1 = time.time() 
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 600 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-screen.fill((255, 0, 0))
-pygame.display.update()
+reaction_times = []
 clock = pygame.time.Clock()
-clock.tick(0.4)
-START = time.time()
-RUN = True
-while RUN:
+for i in range(5):
+    screen.fill((255, 0, 0))
+    pygame.display.update()
+    t = random.randint(3,9)
+    clock.tick(t/10)
+    RUN = True
     screen.fill((0, 255, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUN = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            END = time.time()
-            RUN = False
-    pygame.display.flip()
+    START = time.time()
+    while RUN:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUN = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    END = time.time()
+                    RUN = False                
+        pygame.display.flip()
+    
+    reaction_time = (END - START)*1000
+    if START - t1 < END - t1: 
+        reaction_times.append(reaction_time)
+
 pygame.quit()
 
-REACTION = (END - START)*1000
+REACTION = sum(reaction_times) / len(reaction_times)
 print(REACTION)
 
+reaction_times = np.array(reaction_times)
+diffs = np.diff(reaction_times)
+mean_diff = np.mean(diffs)
+RATE = mean_diff/np.mean(reaction_times) * (-100)
+print(RATE)
 # VARIABLE ETUDIEE
 
 AGE = input("age : ")
@@ -38,14 +55,14 @@ CONSOMMATION_CAFFEINE = input("Consomme tu de la caffeine ?")
 FATIGUE = input("Es-tu fatigué ?")
 SOMMEIL = input("Combien d'heure de sommeil ?")
 REVEIL = input("Combien d'heure depuis le reveil ?")
+TEL = input("Combien d'heure de téléphone?")
 
-
-# 3am men sayyev
+# save to excel
 workbook = openpyxl.load_workbook('Data1.xlsx')
 
 sheet = workbook.active
 
-new_row = (REACTION, AGE, SEXE, RESULTAT_SCOLAIRE, CONSOMMATION_CAFFEINE, FATIGUE, SOMMEIL, REVEIL)
+new_row = (REACTION, AGE, SEXE, RESULTAT_SCOLAIRE, CONSOMMATION_CAFFEINE, FATIGUE, SOMMEIL, REVEIL, TEL)
 sheet.append(new_row)
 
 workbook.save('Data1.xlsx')
